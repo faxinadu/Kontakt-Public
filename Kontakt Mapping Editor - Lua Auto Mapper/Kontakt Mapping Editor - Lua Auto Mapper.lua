a8c2b608b8a90c6d659de4ce4bb05e67c06f560e
Yaron Eshkar <34368899+faxinadu@users.noreply.github.com>
2020-07-27 10:22:55 +0200

    Update Kontakt Mapping Editor - Lua Auto Mapper.lua
    recognise by note too

--- Kontakt Mapping Editor - Lua Auto Mapper/Kontakt Mapping Editor - Lua Auto Mapper.lua
+++ Kontakt Mapping Editor - Lua Auto Mapper/Kontakt Mapping Editor - Lua Auto Mapper.lua
@@ -3,7 +3,7 @@
 Kontakt Mapping Editor - Lua Auto Mapper
 Author: Native Instruments
 Written by: Yaron Eshkar
-Modified: July 26, 2020
+Modified: July 27, 2020
 
     This script will parse information from file names in order to fill a sample mapping.
 
@@ -51,18 +51,18 @@ local printToConsole = true
 -- Set the tokens the samples include and their order. A value of -1 means the token is not used.
 
 -- These tokens determine zone parameters.
-local rootLocation = 2
-local lowKeyLocation = 3
-local highKeyLocation = 4
-local lowVelLocation = 5
-local highVelLocation = 6
+local rootLocation = 3
+local lowKeyLocation = -1
+local highKeyLocation = -1
+local lowVelLocation = -1
+local highVelLocation = -1
 
 -- These tokens determine group placement and name.
 -- Sample name location should basically always be 1 (and exist) or else error handling gets iffy.
 local sampleNameLocation = 1 
 local signalNameLocation = -1
 local articulationLocation = -1
-local roundRobinLocation = 7
+local roundRobinLocation = -1
 
 -- Default values to replace non existing or erroneous tokens.
 local defaultRootValue = 48
@@ -123,6 +123,144 @@ local function print_r(arr)
     end
     print(str)
 end
+
+-- Function for checking how a string starts.
+function string.starts(inputString,startWith)
+   return string.sub(inputString,1,string.len(startWith))==startWith
+end
+
+-- Table with note names.
+local noteNames = {
+"C-2",
+"C#-2",
+"D-2",
+"D#-2",
+"E-2",
+"F-2",
+"F#-2",
+"G-2",
+"G#-2",
+"A-2",
+"A#-2",
+"B-2",
+"C-1",
+"C#-1",
+"D-1",
+"D#-1",
+"E-1",
+"F-1",
+"F#-1",
+"G-1",
+"G#-1",
+"A-1",
+"A#-1",
+"B-1",
+"C0",
+"C#0",
+"D0",
+"D#0",
+"E0",
+"F0",
+"F#0",
+"G0",
+"G#0",
+"A0",
+"A#0",
+"B0",
+"C1",
+"C#1",
+"D1",
+"D#1",
+"E1",
+"F1",
+"F#1",
+"G1",
+"G#1",
+"A1",
+"A#1",
+"B1",
+"C2",
+"C#2",
+"D2",
+"D#2",
+"E2",
+"F2",
+"F#2",
+"G2",
+"G#2",
+"A2",
+"A#2",
+"B2",
+"C3",
+"C#3",
+"D3",
+"D#3",
+"E3",
+"F3",
+"F#3",
+"G3",
+"G#3",
+"A3",
+"A#3",
+"B3",
+"C4",
+"C#4",
+"D4",
+"D#4",
+"E4",
+"F4",
+"F#4",
+"G4",
+"G#4",
+"A4",
+"A#4",
+"B4",
+"C5",
+"C#5",
+"D5",
+"D#5",
+"E5",
+"F5",
+"F#5",
+"G5",
+"G#5",
+"A5",
+"A#5",
+"B5",
+"C6",
+"C#6",
+"D6",
+"D#6",
+"E6",
+"F6",
+"F#6",
+"G6",
+"G#6",
+"A6",
+"A#6",
+"B6",
+"C7",
+"C#7",
+"D7",
+"D#7",
+"E7",
+"F7",
+"F#7",
+"G7",
+"G#7",
+"A7",
+"A#7",
+"B7",
+"C8",
+"C#8",
+"D8",
+"D#8",
+"E8",
+"F8",
+"F#8",
+"G8",
+"G#8"
+}
 
 ----------------------------------------------------------------------------------------------------
 -- A flag that turns to true if there were any parsing errors.
@@ -310,8 +448,14 @@ for index, file in next,samplesPaths do
 
     -- Set the zone root key.
     if rootLocation ~= -1 then
-        -- Remove non numerical characters from the token.
-    	local value = tonumber(samplesTokens[index][rootLocation]:match('%d[%d.,]*'))
+        local value = 0
+        if string.starts(samplesTokens[index][rootLocation],'%d') then
+            -- Remove non numerical characters from the token.
+        	value = tonumber(samplesTokens[index][rootLocation]:match('%d[%d.,]*'))
+        else
+            -- Check for the index value of the note string
+            value = tonumber(tableValueIndex(noteNames,samplesTokens[index][rootLocation]))-1
+        end
     	-- Check that the value is valid and in range.
         if value > -1 and value < 128 then
     		z.rootKey = value
@@ -330,8 +474,14 @@ for index, file in next,samplesPaths do
     else
         -- Set the zone low key.
         if lowKeyLocation ~= -1 then
-            -- Remove non numerical characters from the token.
-        	local value = tonumber(samplesTokens[index][lowKeyLocation]:match('%d[%d.,]*'))
+            local value = 0
+            if string.starts(samplesTokens[index][lowKeyLocation],'%d') then
+                -- Remove non numerical characters from the token.
+            	value = tonumber(samplesTokens[index][lowKeyLocation]:match('%d[%d.,]*'))
+            else
+                -- Check for the index value of the note string
+                value = tonumber(tableValueIndex(noteNames,samplesTokens[index][lowKeyLocation]))-1
+            end
             -- Check that the value is valid and in range.
         	if value > -1 and value < 128 then
         		z.keyRange.low = value
@@ -342,10 +492,17 @@ for index, file in next,samplesPaths do
         		errorFlag = true
         	end			
         end
+       
         -- Set the zone high key.
         if highKeyLocation ~= -1 then
-            -- Remove non numerical characters from the token.
-        	local value = tonumber(samplesTokens[index][highKeyLocation]:match('%d[%d.,]*'))
+            local value = 0
+            if string.starts(samplesTokens[index][highKeyLocation],'%d') then
+                -- Remove non numerical characters from the token.
+            	value = tonumber(samplesTokens[index][highKeyLocation]:match('%d[%d.,]*'))
+            else
+                -- Check for the index value of the note string
+                value = tonumber(tableValueIndex(noteNames,samplesTokens[index][highKeyLocation]))-1
+            end
             -- Check that the value is valid and in range.
         	if value > -1 and value < 128 then
         		z.keyRange.high = value
